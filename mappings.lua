c03f8007dfcf0f03ec7c83cd70c5e4e53f245998
youtirsin <youtirsin@qq.com>
2023-12-17 20:54:41 +0800

    add range format mapping

--- mappings.lua
+++ mappings.lua
@@ -1,6 +1,18 @@
 ---@type MappingsTable
 local M = {}
 
+local range_formatting = function()
+  local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
+  local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
+  vim.lsp.buf.format({
+    range = {
+      ["start"] = { start_row, 0 },
+      ["end"] = { end_row, 0 },
+    },
+    async = true,
+  })
+end
+
 M.general = {
   n = {
     ["<Leader>a"] = {"<cmd>AerialToggle!<CR>", "Open Aerial outline"},
@@ -8,6 +20,7 @@ M.general = {
   v = {
     [">"] = { ">gv", "indent"},
     ["<Leader>y"] = {"\"+y", "Copy to clipboard"},
+    ["f"] = {range_formatting, "LSP range formatting"}
   },
 }
 
