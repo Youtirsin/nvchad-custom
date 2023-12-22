2c546280e4bd30396194b38d97e22bad67f8ff30
youtirsin <youtirsin@qq.com>
2023-12-22 13:24:12 +0800

    disable null-ls warning

--- init.lua
+++ init.lua
@@ -7,3 +7,13 @@
 -- })
 vim.opt.clipboard = ""
 vim.wo.relativenumber = true
+
+-- disable null-ls legitimate warning
+local notify = vim.notify
+vim.notify = function(msg, ...)
+  if msg:match("warning: multiple different client offset_encodings") then
+     return
+  end
+
+  notify(msg, ...)
+end
